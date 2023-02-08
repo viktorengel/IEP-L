256390d05e6c95d4522b30f1927e6cb2ee3ef9ea
Victor Rengel <viktorengel@hotmail.com>
2023-02-08 16:40:37 -0500

    Migracición 1:1

---
+++ database/migrations/2023_02_08_211420_create_personas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id(); //id
+            $table->string('nombres',50);
+            $table->string('apellidos',50)->nullable();
+            $table->string('direccion')->nullable();
+            //1:1
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            
+            $table->timestamps(); // created_at, updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+};
