a573762942c8fc581b6ea4c3b56959c58b950916
Viktor Rengel <viktorengel@hotmail.com>
2023-02-15 22:23:17 -0500

    Fin del video 6

--- database/migrations/2023_02_09_182557_create_marcas_table.php
+++ database/migrations/2023_02_09_182557_create_marcas_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('marcas', function (Blueprint $table) {
             $table->id();
-            $table->string('marca');
+            $table->string('nombre');
             $table->timestamps();
         });
     }
